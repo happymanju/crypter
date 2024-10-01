55b3544745afebe55b692713ae2fa1c3a0167a7b
mpiastucki <michael.piastucki@woven-planet.global>
2024-10-01 12:50:24 +0900

    figure out how to do outputs

--- crypter/crypter.go
+++ crypter/crypter.go
@@ -6,58 +6,105 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 )
+
+type decodeFunc func([]byte) ([]byte, error)
+type encodeFunc func([]byte) ([]byte)
+type modeFuncs struct {
+	encodeFunc
+	decodeFunc
+}
+
+var modesMap map[string]modeFuncs = map[string]modeFuncs {
+	"base64": {toBase64,fromBase64},
+	"hex": {toHex, fromHex},
+}
+
+
 
 // CLI parses command line arguments and executes app with provided parameters, and returns an exit code
 func CLI(args []string) int {
 	encodeFlags := flag.NewFlagSet("encode", flag.ExitOnError)
 	encodeFormat := encodeFlags.String("f", "base64", "format to encode")
-	encodeOutput := encodeFlags.String("o", "stdout", "write encoded output to stdout or file")
+	encodeOutput := encodeFlags.String("o", "", "write encoded output to file")
+	encodeInputFile := encodeFlags.String("i", "", "read contents of file for encode")
 
 	decodeFlags := flag.NewFlagSet("decode", flag.ExitOnError)
 	decodeFormat := decodeFlags.String("f", "base64", "format to decode from")
-	decodeOutput := decodeFlags.String("o", "stdout", "write decoded output to stdout or file")
+	decodeOutput := decodeFlags.String("o", "stdout", "write decoded output to file")
+	decodeInputFile := decodeFlags.String("i", "", "read contents of file for decode")
 
-	var ac AppContext = AppContext{}
-	var encodeMap map[string]func([]byte) []byte
-	encodeMap["base64"] = toBase64
-	encodeMap["hex"] = toHex
 
-	var decodeMap map[string]func(string) ([]byte, error)
-	decodeMap["base64"] = fromBase64String
-	decodeMap["hex"] = fromHexString
 
 	switch args[1] {
 	case "encode":
 		encodeFlags.Parse(args[2:])
-		// TODO
+
+		// TODO: figure out to switch outputs for appContext
+		ac := &appContext{}
+		ac.fromArgs(modesMap[*encodeFormat].encodeFunc,nil,os.Stdout,)
+
+		err := RunApp(ac, encodeFlags.Args())
+		if err != nil {
+			log.Printf("Error running app: %v\n", err)
+			return 1
+		}
+	
+	case "decode":
+		decodeFlags.Parse(args[2:])
+
+		ac.fun = modesMap[*decodeFormat].decodeFunc
+		if *decodeOutput == "stdout" {
+			ac.output = os.Stdout
+		} else {
+			f, err := os.Open(*decodeOutput)
+			if err != nil {
+				log.Printf("Error with -o flag: %v\n", err)
+				return 1
+			}
+			ac.output = f
+		}
+
+		err := RunApp(ac, decodeFlags.Args())
+		if err != nil {
+			log.Printf("Error running app: %v\n", err)
+			return 1
+		}
+	default:
+		fmt.Printf("invalid flags")
+		encodeFlags.Usage()
 
 	}
 
 	return 0
 }
 
-func Run(ac AppContext) error {
+// RunApp - use parsed CLI options to execute app
+func RunApp(ac appContext, tailArgs []string) error {
 	// TODO
+	input := tailArgs[0]
+
 	return nil
 }
 
-type AppContext struct {
-	fun    interface{}
+type appContext struct {
+	enc encodeFunc
+	dec decodeFunc
+	input io.Reader
 	output io.Writer
+	payload []byte
 }
 
-func (app *AppContext) fromArgs(fun interface{}, output io.Writer) *AppContext {
-	app.fun = fun
+func (app *appContext) fromArgs(enc encodeFunc, dec decodeFunc, input string, output io.Writer, payload []byte) *appContext {
+	app.enc = enc
+	app.dec = dec
+	app.input = input
 	app.output = output
+	app.payload = payload
 
 	return app
-}
-
-func (app *AppContext) Run() error {
-	// TODO
-	return nil
 }
 
 func toBase64(src []byte) []byte {
@@ -80,40 +127,24 @@ func fromBase64(src []byte) ([]byte, error) {
 	return decodedData, nil
 }
 
-func fromBase64String(src string) ([]byte, error) {
-	decodedData := make([]byte, base64.StdEncoding.DecodedLen(len(src)))
-
-	decodedData, err := fromBase64([]byte(src))
-	if err != nil {
-		return nil, err
-	}
-	return decodedData, nil
-
-}
-
-func toBase64String(src []byte) string {
-	encodedString := string(toBase64(src))
-	return encodedString
-}
-
 func toHex(src []byte) []byte {
 	var encodedData []byte = make([]byte, hex.EncodedLen(len(src)))
 
 	n := hex.Encode(encodedData, src)
 
-	fmt.Fprintf(os.Stdout, "Encoded %v bytes to hex", n)
+	fmt.Fprintf(os.Stdout, "Encoded %v bytes to hex\n", n)
 
 	return encodedData
 }
 
-func fromHexString(src string) ([]byte, error) {
-	var decodedData []byte = make([]byte, 0)
+func fromHex(src []byte) ([]byte, error) {
+	var decodedData []byte = make([]byte, hex.DecodedLen(len(src)))
 
-	decodedData, err := hex.DecodeString(src)
-
+	n, err := hex.Decode(decodedData,src)
 	if err != nil {
 		return nil, err
 	}
-
+	fmt.Fprintf(os.Stdout, "Decoded %v bytes\n", n)
 	return decodedData, nil
+
 }
